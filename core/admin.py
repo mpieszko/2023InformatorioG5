daa25ea90060bcdc8a62ef4ecf65f23d37826530
Lucas Gabriel Gómez <lucasgabrielg2001@gmail.com>
2023-07-09 14:44:18 -0300

    Creando los modelos y añadiendolos al admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Categoria, Etiqueta, Articulo
 
 # Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(Etiqueta)
+admin.site.register(Articulo)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=200, unique=True)
+    activo = models.BooleanField(default=True)
+    creacion = models.DateTimeField(auto_now_add=True)
+    actualizacion = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['nombre']
+
+    def __str__(self):
+        return self.nombre
+
+class Etiqueta(models.Model):
+    nombre = models.CharField(max_length=200, unique=True)
+    activo = models.BooleanField(default=True)
+    creacion = models.DateTimeField(auto_now_add=True)
+    actualizacion = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['nombre']
+
+    def __str__(self):
+        return self.nombre
+
+class Articulo(models.Model):
+    titulo = models.CharField(max_length=250)
+    bajada = models.CharField(max_length=600)
+    contenido = models.TextField()
+    imagen = models.ImageField(upload_to='articulo', null=True)
+    publicado = models.BooleanField(default=False)
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True)
+    autor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    etiqueta = models.ManyToManyField(Etiqueta)
+    creacion = models.DateTimeField(auto_now_add=True)
+    actualizacion = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-creacion']
+
+    def __str__(self):
+        return self.titulo
